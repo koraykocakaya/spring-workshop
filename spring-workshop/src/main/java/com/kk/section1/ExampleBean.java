6e2cbe8ec4b1a8e656ebcfaed5cc96f565a28650
koray.kocakaya <koray.kocakaya@cybersoft.com.tr>
2022-06-09 10:14:39 +0300

    First Commit

---
+++ spring-workshop/src/main/java/com/kk/section1/ExampleBean.java
@@ -0,0 +1,13 @@
+package com.kk.section1;
+
+public class ExampleBean {
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
