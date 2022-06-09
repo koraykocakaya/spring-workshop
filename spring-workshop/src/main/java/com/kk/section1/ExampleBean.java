beaa5bfd9567a39a60cdcb862b21337c0f619602
koray.kocakaya <koray.kocakaya@cybersoft.com.tr>
2022-06-09 10:16:19 +0300

    First Commit

--- spring-workshop/src/main/java/com/kk/section1/ExampleBean.java
+++ spring-workshop/src/main/java/com/kk/section1/ExampleBean.java
@@ -1,7 +1,13 @@
 package com.kk.section1;
 
+/**
+ * 1. Su anki sistemde otomatik olarak default constructor, sonrasinda set metodu cagrilmakta  
+ * @author korayk
+ */
 public class ExampleBean {
 	private String name;
+	
+
 	
 	public String getName() {
 		return name;
