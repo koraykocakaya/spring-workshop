d1f4f37b3ecf916f02adad0166a9757b2ace3fc3
koraykocakaya <mkoraykocakaya@gmail.com>
2022-08-19 09:20:58 +0300

    Comments updated

--- spring-boot-workshop/src/main/java/com/kk/rest/webservices/springbootworkshop/controller/Controller04DocsActuatorHal.java
+++ spring-boot-workshop/src/main/java/com/kk/rest/webservices/springbootworkshop/controller/Controller04DocsActuatorHal.java
@@ -5,12 +5,12 @@ package com.kk.rest.webservices.springbootworkshop.controller;
  *  Spring Boot'un urettigi OPENAPI specificationa gore swagger dokumani uretecektir
  * 2. Bu proje icin http://localhost:8093/swagger-ui/index.html adresinden erisilebilir
  *  Burada hangi servisler hangi parametrelerle cagriliyor, hangi validationlar var, input output obje tipleri vs belirtilmektedir
- * 3. Actuator ile metrikleri, ehalt check vs. izelyebilmekteyiz. Burada http://localhost:8093/actuator uzerinden erisilebilmektedir
+ * 3. Actuator ile metrikleri, health check vs. izelyebilmekteyiz. Burada http://localhost:8093/actuator uzerinden erisilebilmektedir
  *  http://localhost:8093/actuator/metrics/http.server.requests gibi isteklerle de detaylar goruntulenebilmektedir
  * 4. HAL Explorer ile API'nin detay bilgilerine neler icerdigine vs. erisilebilmektedir
  *  Direkt localhost:8093 adresi uzerinden erisilebilmektedir
  * @author korayk
  */
-public class Controller04 {
+public class Controller04DocsActuatorHal {
 
 }
--- spring-boot-workshop/src/main/java/com/kk/rest/webservices/springbootworkshop/exception/UserNotFoundException.java
+++ spring-boot-workshop/src/main/java/com/kk/rest/webservices/springbootworkshop/exception/UserNotFoundException.java
@@ -3,6 +3,11 @@ package com.kk.rest.webservices.springbootworkshop.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * 1. Bu exception throw edildiginde Status Code NOT_FOUND (404) olmasi icin @ResponseStatus eklendi
+ * @author korayk
+ *
+ */
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException{
 
