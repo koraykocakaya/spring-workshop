f79714f93d2043dd4126a6998aae87f494e18bd3
koraykocakaya <mkoraykocakaya@gmail.com>
2022-09-20 08:44:00 +0300

    Security Config and tests updated

--- user-management/src/main/java/com/kk/user/management/config/SecurityConfig.java
+++ user-management/src/main/java/com/kk/user/management/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.kk.user.management.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,7 +20,7 @@ import org.springframework.security.web.SecurityFilterChain;
  */
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -44,15 +46,22 @@ public class SecurityConfig {
 	}
 	
 	@Bean
-	public UserDetailsService userDetailsService() {
+	public UserDetailsService userDetailsService(AuthenticationManagerBuilder auth) {
 		System.out.println("User details called");
-		UserDetails ud = User.withDefaultPasswordEncoder()
+		UserDetails user1 = User.withDefaultPasswordEncoder()
 				 .username("Koray")
 				 .password("12")
 				 .roles("ADMIN")
 				 .build();
 		
-		return new InMemoryUserDetailsManager(ud);
+		
+		UserDetails user2 = User.withDefaultPasswordEncoder()
+				 .username("admin")
+				 .password("1234")
+				 .roles("ADMIN")
+				 .build();
+		
+		return new InMemoryUserDetailsManager(user1, user2);
 	}
 	
 //	@Bean
